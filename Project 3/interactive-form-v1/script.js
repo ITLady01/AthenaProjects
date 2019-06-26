38400bded49d3758aacc63f5848d1dc35c62dfd0
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-06-26 16:50:29 -0400

    starting projecting and will continue to update

---
+++ Project 3/interactive-form-v1/script.js
@@ -0,0 +1,8 @@
+$('#name').focus(function() {
+    alert( "Please enter your Job Role() called.");
+let jobRoles = document.getElementById("title");
+$('#design').hide();
+
+});
+console.log(design);
+
