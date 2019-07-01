581db076ca977e45c9637bad6701f3c2a71a8da7
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-01 17:18:23 -0400

    Adding Project 4 files

--- Project 3/interactive-form-v1/script.js
+++
@@ -1,8 +0,0 @@
-$('#name').focus(function() {
-    alert( "Please enter your Job Role() called.");
-let jobRoles = document.getElementById("title");
-$('#design').hide();
-
-});
-console.log(design);
-
