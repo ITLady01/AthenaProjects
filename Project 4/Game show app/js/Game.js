458418abe696852177ae1200042bd6417f649543
nwokiwu <51241053+nwokiwu@users.noreply.github.com>
2019-07-08 15:55:38 -0400

    Updated files

--- Project 4/Game show app/js/Game.js
+++ Project 4/Game show app/js/Game.js
@@ -1,3 +1,26 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+const game = new Game();
+
+console.log(game);
+startGame();
+
+getRandomPhrase();
+addPhraseToDisplay();
+handleInteraction();
+removeLife();
+showMatchedLetter();
+checkForWin();
+gameOver();
+
+// missed Game {
+//    missed Game() {
+//    }
+
+//    public Puppy(String name) {
+//       // This constructor has one parameter, name.
+//    }
+// }
--- Project 4/Game show app/js/Phrase.js
+++ Project 4/Game show app/js/Phrase.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+const phrase = new Phrase();
+addPhraseToDisplay();
+showMatchedLetter(); 
+checkLetter();
+console.log(phrase);
+
+
+
+
+
+
+
+
+// public class phrase {
+//     public Puppy(String name) {
+//         // This constructor has one parameter, name.
+//         System.out.println("Passed Name is :" + name);
+//     }
+
+//     public static void main(String []args) {
+//         // Following statement would create an object myPuppy
+//         Puppy myPuppy = new Puppy("tommy");
+//     }
+// }
